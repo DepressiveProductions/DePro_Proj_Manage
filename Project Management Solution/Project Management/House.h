38488dda485ace1565a7395039821151908c6c98
Emil Jacobsen <emil@depressiveproductions.se>
2012-11-29 18:16:51 +0100

    Polish and commenting of the House class

--- Project Management Solution/Project Management/House.h
+++ Project Management Solution/Project Management/House.h
@@ -15,38 +15,34 @@ class House
 		House(void);
 		~House(void);
 		
-		void init(GLfloat fRadius);
-		void init(GLfloat fRadius, M3DVector4f shine, M3DVector4f color);
-		void create(vector< vector<float> > &pos);
-		void draw(MyShaderManager &emilShaders, GLGeometryTransform &tPipeline, M3DVector4f vLightPos, GLMatrixStack &mvStack, M3DVector4f vAmbient, GLBatch &batch);
-		void drawAll(MyShaderManager &emilShaders, GLGeometryTransform &tPipeline, M3DVector4f vLightPos, GLMatrixStack &mvStack, M3DVector4f vAmbient);
-		void inherit(GLfloat fRadius, M3DVector4f shine, M3DVector4f color);
-		void nextPos();
+		void init(GLfloat fRadius); //Initiate a building type with default shinyness and color
+		void init(GLfloat fRadius, M3DVector4f shine, M3DVector4f color); //Initiate a building with specified shinyness and color
+		void create(vector< vector<float> > &pos); //Create a building - not a building type, a factual building
+		void draw(MyShaderManager &emilShaders, GLGeometryTransform &tPipeline, M3DVector4f vLightPos, GLMatrixStack &mvStack, M3DVector4f vAmbient, GLBatch &batch); //Draw building
+		void drawAll(MyShaderManager &emilShaders, GLGeometryTransform &tPipeline, M3DVector4f vLightPos, GLMatrixStack &mvStack, M3DVector4f vAmbient); //Loop through every building of the building type, and draw them
+		void inherit(GLfloat fRadius, M3DVector4f shine, M3DVector4f color); //Used to make buildings inherit values from their building type
 
-		void setColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a);
-		void setColor(M3DVector4f &vCustColor, GLfloat r, GLfloat g, GLfloat b, GLfloat a);
-		void setColor(M3DVector4f &vOldColor, M3DVector4f vNewColor);
+		void setColor(GLfloat r, GLfloat g, GLfloat b, GLfloat a); //Set building type color to specified GLfloats
+		void setColor(M3DVector4f &vOldColor, GLfloat r, GLfloat g, GLfloat b, GLfloat a); //Set a specified color to specified GLfloats
+		void setColor(M3DVector4f &vOldColor, M3DVector4f vNewColor); //Set specified color to specified M3DVector4f
 		
-		void getColor(M3DVector4f &vOut);
-		void getPositions(M3DVector4f &vOutStarting, M3DVector4f &vOutEnding);
-		float getRadius();
+		void getColor(M3DVector4f &vOut); //Returns the building type color by setting it to &vOut
+		float getRadius(); //Returns the radius of the cubes
+		vector< vector<float> > getPositions(); //Returns the positions of the cubes that make up the building
+		vector< House * > getHouses(); //Returns the buildings of the building type
 	
 	private:
-		M3DVector4f vColor;
+		M3DVector4f vColor; //Color of the building
 
-		M3DVector4f vSpecular;
+		M3DVector4f vSpecular; //Shinyness of the building
 
-		M3DVector4f vStartingPos;
-		M3DVector4f vEndingPos;
-		M3DVector3f vCurrentPos;
+		GLBatch houseBatch; //Batch to draw
+		GLFrame houseFrame; //Frame used in the drawing loop
+
+		vector< House * > houses; //List of buildings of the building type
+		vector< vector< float > > positions; //List of positions of the cubes that make up the building
 		
-		GLBatch houseBatch;
-		GLFrame houseFrame;
-
-		vector<House *> houses;
-		vector< vector<float> > positions;
-		
-		GLfloat radius;
-		unsigned int cubes;
+		GLfloat radius; //Radius of cubes that make up the buildings
+		unsigned int cubes; //Used in the drawing method, represents number of cubes that make up a building
 };
 
