a779d518900def241dcc51a229444801a63f03fa
nevvi <jonas@pysyte.se>
2012-12-01 16:41:09 +0100

    Atleast less buggy

--- Project Management Solution/Project Management/Character.cpp
+++ Project Management Solution/Project Management/Character.cpp
@@ -13,11 +13,11 @@ void Character::init(float charSize, float mvSpeed, M3DVector3f &spawnPos)
 {
 	size = charSize;
 	cFrame.SetOrigin(spawnPos);
-	stepSize = 0.01f * mvSpeed;
+	stepSize = 0.1f;// * mvSpeed;
 	moving = false;
 
-	gltMakeCube(cBatch, charSize);
-	vColour[0] = 0.0f;
+	gltMakeCube(cBatch, size);
+	vColour[0] = 1.0f;
 	vColour[1] = 0.0f;
 	vColour[2] = 0.0f;
 	vColour[3] = 1.0f;
@@ -30,14 +30,15 @@ void Character::moveTo(M3DVector3f mvPos)
 
 	do
 	{
-		cFrame.GetOrigin(position);
 		calculateDirection(mvPos);
 		cFrame.SetUpVector(direction);
 		cFrame.MoveUp(stepSize);
 		std::cout << cFrame.GetOriginX() << ", " << cFrame.GetOriginY() << std::endl;
 		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+		cFrame.GetOrigin(position);
 	}
-	while (position != mvPos);
+	while ((position[0] >= mvPos[0]-stepSize) && (position[0] <= mvPos[0]+stepSize)
+			&& (position[1] >= mvPos[2]-stepSize) && (position[1] <= mvPos[1]+stepSize));
 	moving = false;
 }
 
