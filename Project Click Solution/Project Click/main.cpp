d1fcd321c4cc9afbf16b99f6d72c462ae994ebee
Emil Jacobsen <emil@depressiveproductions.se>
2013-01-20 18:32:41 +0100

    Main.cpp
    Nicely structured

--- Project Click Solution/Project Click/main.cpp
+++ Project Click Solution/Project Click/main.cpp
@@ -1,7 +1,193 @@
-#include <GLTools.h>
+//OpenGL Includes:
 
-void setupRC();											//One-time setup function (RC = Rendering Context)
+#include <GLTools.h>
+#include <GLShaderManager.h>
+#include <GLMatrixStack.h>
+#include <GLBatch.h>
+#include <GLFrame.h>
+#include <GLFrustum.h>
+#include <GLGeometryTransform.h>
+
+#ifdef __APPLE__
+#include <glut/glut.h>
+#else
+#define FREEGLUT_STATIC
+#include <GL/glut.h>
+#endif
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//Other includes:
+
+#include <math.h>
+#include <ctime>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <array>
+
+//GLTools objects:
+
+GLGeometryTransform tPipeline;
+GLShaderManager gltShaderManager;
+GLMatrixStack projectionStack;
+GLMatrixStack modelViewStack;
+GLFrame cameraFrame;
+GLFrustum viewFrustum;
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//Objects:
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//Global vars:
+
+#define W_WIDTH 1280
+#define W_HEIGHT 720
+#define W_TITLE "Project Click - Alpha"
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//Function primitives:
+
+void setup();											//One-time setup function
 void changeSize(int w, int h);							//Runs everytime the window 'changes size', for example when the window is created
 void renderScene();										//Basic glutfunc for rendering stuff. Runs every frame
-void handleInput();										//Handles input ...
-void clickFunc();
+void handleInput();										//Handles keyboard input
+void clickFunc(int key, int state, int x, int y);		//Handles mouse clicking
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//Function definitions:
+
+void setup()
+{
+	//Background:
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	//Enable depth testing and multisampling, set blending function:
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_MULTISAMPLE);
+	glBlendFunc(GL_SRC_ALPHA, GL_SRC_COLOR);
+
+	//Initialize GLTools' stock shaders:
+	gltShaderManager.InitializeStockShaders();
+
+	//Initialize custom shaders:
+	//Comming soon
+
+	//Initiate game layer:
+	//Comming soon
+
+	//Set initial camera position:
+	cameraFrame.MoveForward(-20.0f);
+	cameraFrame.MoveUp(-3.5f);
+	cameraFrame.RotateWorld(0.15f, 1.0f, 0.0f, 0.0f);
+
+	//More initiations below here ...
+}
+
+void changeSize(int w, int h)
+{
+	//Tell OpenGL about the new window size:
+	glViewport(0, 0, w, h);
+
+	//Don't divide by zero:
+	if (h = 0) h = 1;
+
+	//Tell GLTools about the new window size:
+	viewFrustum.SetPerspective(35.0f, float(w)/float(h), 1.0f, 100.0f);
+	projectionStack.LoadMatrix(viewFrustum.GetProjectionMatrix());
+	tPipeline.SetMatrixStacks(modelViewStack, projectionStack);
+}
+
+void renderScene()
+{
+	//Clear buffers:
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	//Beginning push:
+	modelViewStack.PushMatrix();
+
+	//Camera matrix:
+	M3DMatrix44f mCamera;
+	cameraFrame.GetCameraMatrix(mCamera);
+	modelViewStack.PushMatrix(mCamera);
+
+	//Light source:
+	static M3DVector4f vAmbient = {0.1f, 0.1f, 0.1f, 1.0f};
+	static M3DVector4f vLightPos = {0.0f, 0.0f, 4.0f, 1.0f};
+	static M3DVector4f vLightEyePos;
+	m3dTransformVector4(vLightEyePos, vLightPos, mCamera);
+
+	//Render stuff here:
+	//
+
+	//Camera matrix pop:
+	modelViewStack.PopMatrix();
+
+	//Ending pop:
+	modelViewStack.PopMatrix();
+
+	//Swap buffers and tell glut to keep looping:
+	glutSwapBuffers();
+	glutPostRedisplay();
+
+	//Processor heavy:
+	
+	//Input:
+	//
+
+	//Game layer:
+	//
+}
+
+void handleInput() 
+{
+
+}
+
+void clickFunc(int key, int state, int x, int y) 
+{
+
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//Main:
+
+int main(int argc, char* argv[]) //Starting point of the application
+{
+	//Initiate GLTools:
+	gltSetWorkingDirectory(argv[0]);
+
+	//Initiate glut and create a window:
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE);
+	glutInitWindowSize(W_WIDTH, W_HEIGHT);
+	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-W_WIDTH)/2,
+							(glutGet(GLUT_SCREEN_HEIGHT)-W_HEIGHT)/2); //Center of screen
+	glutCreateWindow(W_TITLE);
+
+	//Send funcs to glut:
+	glutReshapeFunc(changeSize);
+	glutDisplayFunc(renderScene);
+	glutMouseFunc(clickFunc);
+
+	//Initiate GLEW, quit if it doesn't work:
+	GLenum result = glewInit();
+	if(result != GLEW_OK)
+	{
+		fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(result));
+		return 1;
+	}
+
+	//Run setup and start looping:
+	setup();
+	glutMainLoop();
+	system("PAUSE");
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
