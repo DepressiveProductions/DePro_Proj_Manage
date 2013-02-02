53b6a93c8d22a12b18ef9ee717c48f38f861cc14
Emil Jacobsen <emil@depressiveproductions.se>
2013-02-02 15:59:39 +0100

    Fixed crash from Jonas commit

--- Project Click Solution/Project Click/Blocks.h
+++ Project Click Solution/Project Click/Blocks.h
@@ -10,6 +10,7 @@ class Blocks
 public:
 	Blocks(void);
 	~Blocks(void);
+	void init();
 	void sendWave(int blockAmount);
 	void draw(Shaders &sManager, GLGeometryTransform &tPipeline, GLMatrixStack &mvStack, M3DVector4f vLight, M3DVector4f vAmbient);
 	
