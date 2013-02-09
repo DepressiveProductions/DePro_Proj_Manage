4acf59a4b6dc52306c19f99b16901ef1f8a022a1
Emil Jacobsen <emil@depressiveproductions.se>
2013-02-10 00:43:03 +0100

    Cleanup
    Anton:
    Ett c++ program startar i main(), därför kan du inte ha statements där
    ute i global namespace eftersom de inte kommer köras typ

--- Project Click Solution/Project Click/main.cpp
+++ Project Click Solution/Project Click/main.cpp
@@ -92,7 +92,6 @@ M3DMatrix44f			mOrtho;
 M3DMatrix44f			mIdentity;
 
 vector< std::string > fontStrings;
-//fontStrings.push_back("Assets/FONT/TEXT/FONT_ALPHA_A.tga");	//EMIL DEN HÄR FINNS I SETUPPPPPPPP
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -118,48 +117,7 @@ void menuRender();
 //One-time setup function:
 void setup()
 {
-	/*HEJ EMIL JAG VET INTE VAD JAG SKA SKRIVA HER MEN DET FUNKER INTE UTANFER SOM DER UPPA 
-quu..__
- $$$b  `---.__
-  "$$b        `--.                          ___.---uuudP
-   `$$b           `.__.------.__     __.---'      $$$$"              .
-     "$b          -'            `-.-'            $$$"              .'|
-       ".                                       d$"             _.'  |
-         `.   /                              ..."             .'     |
-           `./                           ..::-'            _.'       |
-            /                         .:::-'            .-'         .'
-           :                          ::''\          _.'            |
-          .' .-.             .-.           `.      .'               |
-          : /'$$|           .@"$\           `.   .'              _.-'
-         .'|$u$$|          |$$,$$|           |  <            _.-'
-         | `:$$:'          :$$$$$:           `.  `.       .-'
-         :                  `"--'             |    `-.     \
-        :##.       ==             .###.       `.      `.    `\
-        |##:                      :###:        |        >     >
-        |#'     `..'`..'          `###'        x:      /     /
-         \                                   xXX|     /    ./
-          \                                xXXX'|    /   ./
-          /`-.                                  `.  /   /
-         :    `-  ...........,                   | /  .'
-         |         ``:::::::'       .            |<    `.
-         |             ```          |           x| \ `.:``.
-         |                         .'    /'   xXX|  `:`M`M':.
-         |    |                    ;    /:' xXXX'|  -'MMMMM:'
-         `.  .'                   :    /:'       |-'MMMM.-'
-          |  |                   .'   /'        .'MMM.-'
-          `'`'                   :  ,'          |MMM<
-            |                     `'            |tbap\
-             \                                  :MM.-'
-              \                 |              .''
-               \.               `.            /
-                /     .:::::::.. :           /
-               |     .:::::::::::`.         /
-               |   .:::------------\       /
-              /   .''               >::'  /
-              `',:                 :    .'*/
-                                    
 	fontStrings.push_back("Assets/FONT/TEXT/FONT_ALPHA_A.tga");
-	//fontStrings.push_back("Assets/FONT/TEXT/FONT_ALPHA_B.tga");		//Får debug assertion fail, ska sova nu
 
 	//Background:
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -215,6 +173,8 @@ void shutdownRC()
 	survivalButton.clearTexture();
 	optionsButton.clearTexture();
 	exitButton.clearTexture();
+
+	font.clearTexture();
 }
 
 //Runs everytime the window 'changes size', for example when the window is created:
