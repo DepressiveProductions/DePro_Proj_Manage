585d103719ef9e1c4c344acdf3ba7f2d38d5c366
Emil Jacobsen <emil@depressiveproductions.se>
2013-02-18 19:57:33 +0100

    Started refactoring - again ...

--- Project Click Solution/Project Click/Font.h
+++ Project Click Solution/Project Click/Font.h
@@ -24,13 +24,14 @@ public:
 		char	c;
 	};
 
-	void init(map<char,std::string> texFileNames);
+	void init(const char * imagepath);
 	void showText(std::string text, float x, float y, float width, float height = 0.0f);
 	void removeText(std::string key);
 	void initiateBatch(letter *l, float x, float y, float width, float height);
 	void drawAll(GLGeometryTransform pipeline, GLShaderManager &shaderManager);
 	bool loadTGATexture(const char *szFileNames, GLenum minFilter, GLenum magFilter, GLenum wrapMode);
 	void clearTexture(void); // Goes in main before terminating program
+	GLuint loadDDS(const char * imagepath);
 
 private:
 	map< std::string, vector< letter * >> allText;
